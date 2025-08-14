3dced562bd1ad5d28e3e5a3e2f572e13a5a0e164
Erick Pac <erickj.pac@gmail.com>
2025-08-14 12:43:08 -0600

    Add environment variable setup for testing in setup.ts

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -60,3 +60,9 @@ jest.mock('../modules/prisma/prisma.service', () => {
 // Global test environment setup
 process.env.NODE_ENV = 'test';
 process.env.PORT = '3001';
+process.env.RESEND_API_KEY = 'RESEND_API_KEY';
+process.env.CLOUDINARY_CLOUD_NAME = 'CLOUDINARY_CLOUD_NAME';
+process.env.CLOUDINARY_API_KEY = 'CLOUDINARY_API_KEY';
+process.env.CLOUDINARY_API_SECRET = 'CLOUDINARY_API_SECRET';
+process.env.DATABASE_URL = 'DATABASE_URL';
+process.env.JWT_SECRET = 'JWT_SECRET';
